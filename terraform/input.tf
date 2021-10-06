96f8f0c15c3cc7786e1a4b4d82a0f1bfedb82320
robert nick <robertnick04@gmail.com>
2021-10-06 16:54:15 -0400

    updated project files

--- terraform/input.tf
+++ terraform/input.tf
@@ -10,7 +10,21 @@ variable "resource_group" {}
 variable "application_type" {}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable "virtual_network_name" {}
+variable "address_prefix_test" {}
+variable "address_space" {}
+
+# VM
+variable "admin_username" {
+  description = "The admin user name in the VM."
+  default     = "udacity"
+}
+
+variable "packer_image" {
+  description = "The ID of the image created by packer tool."
+}
+
+variable "public_key_path" {
+  description = "Full path incl. filename to the public key."
+}
 
